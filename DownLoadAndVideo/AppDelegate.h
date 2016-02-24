8708136804951cff725789eb62241f4e5931efca
David <zhaoweizheng66@163.com>
2016-02-24 12:00:02 +0800

    DownLoadAndVideo
    视频播放器和断点下载

---
+++ DownLoadAndVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DownLoadAndVideo
+//
+//  Created by mistong on 16/2/24.
+//  Copyright © 2016年 赵伟争. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
